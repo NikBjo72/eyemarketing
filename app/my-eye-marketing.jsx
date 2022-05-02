4985193ce68cee8ee2f23ab501535c742b417ae7
NikBjo72 <niklas@nbjork.se>
2022-05-02 16:08:31 +0200

    Justerat blinking-eye-knapparna lite, så det finns två storlekar

--- app/my-eye-marketing.jsx
+++ app/my-eye-marketing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet, Link } from "react-router-dom";
+import { BlinkingEyeBtn } from './Components/blinking-eye-btn';
 
 export class MyEyeMarketing extends React.Component {
 
@@ -13,16 +14,17 @@ export class MyEyeMarketing extends React.Component {
     this.props.background(true);
   }
 
+  handleClick = (btnName, btnState) => {
+
+  }
+
   render() {
     console.log('logoutRender');
     return (
       <div id="myEyeMarketing">
         <div id="menuContainer">
-          <Link to="/browser"><button id="navBtn">Browser</button></Link>
-          <Link to="/browser"><button id="navBtn">Browser</button></Link>
-          <Link to="/browser"><button id="navBtn">Browser</button></Link>
-          <Link to="/browser"><button id="navBtn">Browser</button></Link>
-          <Link to="/browser"><button id="navBtn">Browser</button></Link>
+          <Link id="navLink" to="/browser"><button id="navBtn"><BlinkingEyeBtn id="smallBtn" text="BROWSER" onClick = {this.handleClick}/></button></Link>
+
           <div id="logoutBtnContainer">
             <Link to="/"><button onClick = {(e) => this.props.onClick(false)} id="logoutBtn">Logga ut</button></Link>
           </div>    
